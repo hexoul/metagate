3026c6090d0f93cbffbd0bb3d532ff6e75af0921
hexoul <crosien@gmail.com>
2018-10-15 15:42:08 +0900

    set initial page rendering with tabs

--- src/App.js
+++ src/App.js
@@ -1,65 +1,19 @@
 import React from 'react';
-import { Form, Select, InputNumber, DatePicker, Switch, Slider, Button } from 'antd';
+import { Tabs } from 'antd';
 import './App.css';
 
-const { Option } = Select;
+const TabPane = Tabs.TabPane;
+
+function callback(key) {
+  console.log(key);
+}
 
 const App = () => (
-  <Form>
-    <Form.Item
-      label="数字输入框"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 8 }}
-    >
-      <InputNumber min={1} max={10} defaultValue={3} />
-      <span className="ant-form-text"> 台机器</span>
-      <a href="https://ant.design">链接文字</a>
-    </Form.Item>
-    <Form.Item
-      label="开关"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 8 }}
-    >
-      <Switch defaultChecked />
-    </Form.Item>
-    <Form.Item
-      label="滑动输入条"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 8 }}
-    >
-      <Slider defaultValue={70} />
-    </Form.Item>
-    <Form.Item
-      label="选择器"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 8 }}
-    >
-      <Select defaultValue="lucy" style={{ width: 192 }}>
-        <Option value="jack">jack</Option>
-        <Option value="lucy">lucy</Option>
-        <Option value="disabled" disabled>disabled</Option>
-        <Option value="yiminghe">yiminghe</Option>
-      </Select>
-    </Form.Item>
-    <Form.Item
-      label="日期选择框"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 8 }}
-    >
-      <DatePicker />
-    </Form.Item>
-    <Form.Item
-      style={{ marginTop: 48 }}
-      wrapperCol={{ span: 8, offset: 8 }}
-    >
-      <Button type="primary" htmlType="submit">
-        确定
-      </Button>
-      <Button style={{ marginLeft: 8 }}>
-        取消
-      </Button>
-    </Form.Item>
-  </Form>
+  <Tabs defaultActiveKey="1" onChange={callback}>
+    <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+    <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+    <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+  </Tabs>
 );
 
 export default App;
