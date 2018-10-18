f4336c4659c222d29a56e089cecb7246eb677ac3
hexoul <crosien@gmail.com>
2018-10-18 18:12:07 +0900

    refactor: components/Topic.js

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -88,25 +88,6 @@ class Topic extends React.Component {
   showModal = (record, type) => {
     console.log('showModal: ', record, type);
     switch(type) {
-      case 'info':
-        this.setState({
-          topicID: record.topicID,
-          issuer: record.issuer,
-          title: record.title,
-          explanation: record.explanation,
-          registerDate: record.registerDate,
-          infoModalVisible: true,
-          addModalVisible: false,
-          qrModalVisible: false,
-        });
-        break;
-      case 'add':
-        this.setState({
-          infoModalVisible: false,
-          addModalVisible: true,
-          qrModalVisible: false,
-        });
-        break;
       case 'qr':
         this.setState({
           newTopicId: newTopicData['topic'],
@@ -119,14 +100,6 @@ class Topic extends React.Component {
         break;
       default: break;
     }
-  }
-
-  handleClose = (e) => {
-    this.setState({
-      infoModalVisible: false,
-      addModalVisible: false,
-      qrModalVisible: false,    
-    });
   }
 
   handleSorting = (e) => {
@@ -188,53 +161,45 @@ class Topic extends React.Component {
           <h3 style={{ margin: '10px 0' }}>Creator(Title / MetaID) : {record.issuer} / 0x7304f14b0909640acc4f6a192381091eb1f37701</h3>
         </div>
       ),
-      onOk() {},
+      onOk() {
+        console.log('OK');
+      },
     });
   }
 
   getModalAddTopic() {
     return <Modal
-      width={500}
-      title={'Add New Topic'}
+      width='70%'
+      title='Add New Topic'
       visible={this.state.addModalVisible}
-      onOk={this.handleOk}
-      onCancel={this.handleCancel}
-      footer={null}
+      okText='Add'
+      onOk={() => this.setState({ addModalVisible: false })}
+      onCancel={() => this.setState({ addModalVisible: false })}
+      closable={false}
       >
         <Form layout='vertical'>
-          <Form.Item
-            label="Title">
+          <Form.Item label='Title'>
             <Input
-              onChange={this.handleChange} 
+              onChange={this.handleChange}
               id='title'
-              placeholder="Input Title"/>
+              placeholder="Input Title"
+            />
           </Form.Item>
-          <Form.Item
-            label="Topic No"
-            >
-            <Input 
-              onChange={this.handleChange} 
+          <Form.Item label='Topic No'>
+            <Input
+              onChange={this.handleChange}
               id='topic'
-              placeholder="Input Topic No or"/>
-              <a style={{ float: 'right', color: 'red' }}>* No. in use / choose different No.</a>
+              placeholder='Input Topic No or'
+            />
+            <a style={{ float: 'right', color: 'red' }}>* No. in use / choose different No.</a>
           </Form.Item>
-          <Form.Item
-            label="Explanation">
-            <Input.TextArea 
+          <Form.Item label='Explanation'>
+            <Input.TextArea
               onChange={this.handleChange} 
-              placeholder="Enter Explanation (max. 32 bytes)" 
+              placeholder='Enter Explanation (max. 32 bytes)'
               autosize={{ minRows: 2, maxRows: 6 }}
-              id='explanation'/>
-          </Form.Item>
-          <Form.Item>
-            <center>
-              <Button 
-                type='primary'
-                size='large'
-                onClick={()=>this.showModal('none','qr')}> 
-                  Add
-              </Button>
-            </center>
+              id='explanation'
+            />
           </Form.Item>
         </Form>
     </Modal>;
@@ -244,24 +209,22 @@ class Topic extends React.Component {
     return (
       <div>
         <div>
-          <Button 
-            type="primary"
+          <Button
+            type='primary'
             size='large'
-            onClick={()=>this.showModal('none','add')}> Add New Topic </Button>
+            onClick={() => this.setState({ addModalVisible: true })}>Add New Topic</Button>
           <Input.Search
-            placeholder="Search by Creator, No., Keyword"
+            placeholder='Search by Creator, No., Keyword'
             onSearch={value => this.onSearch(value)}
             enterButton
             style={{ width: 500, float: 'right', marginBottom: '20px' }}
-        />
+          />
         </div>
         <Radio.Group style={{margin: '10px 10px 0 0'}} onChange={this.handleSorting}>
           <Radio.Button value='1'>All</Radio.Button>
           <Radio.Button value='2'>Pre-fixed</Radio.Button>
           <Radio.Button value='3'>Added</Radio.Button>
         </Radio.Group>
-        <br />
-        {this.getModalAddTopic()}
         <br />
         <Table
           rowKey={record => record.uid}
@@ -271,6 +234,7 @@ class Topic extends React.Component {
           columns={columns}
           dataSource={this.state.data}
         />
+        {this.getModalAddTopic()}
       </div>
     );
   }
