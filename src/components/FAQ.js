35f802dc5a99a51492ae938ebccc67e84b321061
hexoul <crosien@gmail.com>
2018-10-19 16:30:02 +0900

    FAQ: add Input.Search component

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Input } from 'antd';
 
 class FAQ extends React.Component {
+  onSearch(value) {
+
+  }
+
   render() {
     return (
       <div>
-        FAQ
+        <h1><b>FAQ</b></h1>
+        <br />
+        <Input.Search
+          placeholder='Search by Topic, Keywords or Phrase'
+          onSearch={value => this.onSearch(value)}
+          enterButton
+          style={{ width: '80%', float: 'left', marginBottom: '20px' }}
+        />
+        <br />
       </div>
     );
   }
