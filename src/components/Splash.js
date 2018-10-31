6a7204347280cdf21ecf2971ed38f450f1f099ef
hexoul <crosien@gmail.com>
2018-10-31 14:50:29 +0900

    splash: improve rendering

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -7,10 +7,9 @@ class Splash extends React.Component {
       <center>
         <img alt='logo' src='https://github.com/METADIUM/metadium-token-contract/blob/master/misc/Metadium_Logo_Vertical_PNG.png?raw=true' />
         <br/><h1><b>METAGATE</b></h1><br />
-        <h2>Search, Identify and Register</h2><br />
-        <div onClick={() => this.props.onClick()}>
-          <h2><p style={{ color: 'blue' }}>How to use?</p></h2>
-        </div>
+        <h2>Search, Identify and Register<br />
+        {/* eslint-disable-next-line */}
+        <a onClick={() => this.props.onClick()}>How to use?</a></h2>
       </center>
     );
   }
